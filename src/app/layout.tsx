1a31791d9b1e37c14039b1f066e924ea177d97f8
Samilla Soarez <soarezsamilla@gmail.com>
2026-04-01 03:47:04 +0000

    Adicione esse Script da Utmiy: <script>   window.pixelId = "69cc94faa25

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,16 @@ export default function RootLayout({
             fbq('track', 'PageView');
           `}
         </Script>
+        <Script id="utmify-pixel" strategy="afterInteractive">
+          {`
+            window.pixelId = "69cc94faa257f23368ce17ef";
+            var a = document.createElement("script");
+            a.setAttribute("async", "");
+            a.setAttribute("defer", "");
+            a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
+            document.head.appendChild(a);
+          `}
+        </Script>
       </head>
       <body className="font-body antialiased">
         <noscript>
