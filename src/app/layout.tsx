839a94c5978d362a71cb53fcf1a3530a00d8e6eb
Samilla Soarez <soarezsamilla@gmail.com>
2026-04-11 02:34:46 +0000

    agora remova esse outro aqui: <script src="https://cdn.utmify.com.br/scr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,12 +33,6 @@ export default function RootLayout({
             fbq('track', 'PageView');
           `}
         </Script>
-        <Script
-          src="https://cdn.utmify.com.br/scripts/utms/latest.js"
-          data-utmify-prevent-xcod-sck
-          data-utmify-prevent-subids
-          strategy="afterInteractive"
-        />
       </head>
       <body className="font-body antialiased">
         <noscript>
