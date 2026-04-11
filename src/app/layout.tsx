eaad3540f2c290b8387d68e95bf19466562928ce
Samilla Soarez <soarezsamilla@gmail.com>
2026-04-11 02:33:48 +0000

    remova esse codigo aqui da página: <script> window.pixelId = "69cc94faa2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,16 +33,6 @@ export default function RootLayout({
             fbq('track', 'PageView');
           `}
         </Script>
-        <Script id="utmify-pixel-1" strategy="afterInteractive">
-          {`
-            window.pixelId = "69cc94faa257f23368ce17ef";
-            var a = document.createElement("script");
-            a.setAttribute("async", "");
-            a.setAttribute("defer", "");
-            a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
-            document.head.appendChild(a);
-          `}
-        </Script>
         <Script
           src="https://cdn.utmify.com.br/scripts/utms/latest.js"
           data-utmify-prevent-xcod-sck
