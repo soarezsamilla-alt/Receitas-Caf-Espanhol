402ca5902eb3bab79e44a4e58c7f1508d6981acf
Samilla Soarez <soarezsamilla@gmail.com>
2026-04-13 01:03:18 +0000

    Adicione esse outro Script da Utmiy: <script   src="https://cdn.utmify.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,6 +54,15 @@ export default function RootLayout({
             document.head.appendChild(a);
           `}
         </Script>
+
+        {/* Utmify UTMS Script */}
+        <Script 
+          id="utmify-utms"
+          src="https://cdn.utmify.com.br/scripts/utms/latest.js"
+          data-utmify-prevent-xcod-sck
+          data-utmify-prevent-subids
+          strategy="afterInteractive"
+        />
       </head>
       <body className="font-body antialiased">
         {children}
